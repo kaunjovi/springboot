236be442118ac9e0eeb3724a0e511fd2c0385657
kaunjovi <kaunjovi@gmail.com>
2016-08-15 23:45:58 +0530

    Something is wrong. We are getting some Whitelist error.

--- src/main/java/code/kata/springboot/MagicalMainClass.java
+++ src/main/java/code/kata/springboot/MagicalMainClass.java
@@ -2,12 +2,22 @@ package code.kata.springboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class MagicalMainClass {
 	private final static Logger logger = LoggerFactory
 			.getLogger("code.kata.springboot.MagicalMainClass");
 
 	public static void main(String[] args) {
 		logger.debug("Hello world.");
+
+		// This is the magic sauce that will bootstrap the spring application.
+		SpringApplication.run(MagicalMainClass.class, args);
 	}
 }
