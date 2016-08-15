1163e938c8bb74e18af6ff36e80565134e554b34
kaunjovi <kaunjovi@gmail.com>
2016-08-15 13:24:02 +0530

    The vanilla old main class is working.

---
+++ src/main/java/code/kata/springboot/MagicalMainClass.java
@@ -0,0 +1,13 @@
+package code.kata.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicalMainClass {
+	private final static Logger logger = LoggerFactory
+			.getLogger("code.kata.springboot.MagicalMainClass");
+
+	public static void main(String[] args) {
+		logger.debug("Hello world.");
+	}
+}
